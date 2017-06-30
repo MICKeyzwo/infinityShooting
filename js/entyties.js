9afc6a5cf54f3c385b0ac41c04d4681aedf1a740
MICKeyzwo <singing.programmer@gmail.com>
2017-07-01 05:54:09 +0900

    ichiou kansei

--- js/entyties.js
+++ js/entyties.js
@@ -24,6 +24,12 @@ window._setEntyties = function(source, ctx, ent){
     player.prototype.draw = function(){
         ctx.drawImage(this.img, this.x - 25, this.y - 25);
     }
+    player.prototype.hitAmmo = function(ent){
+        return Math.sqrt(Math.pow(this.x - ent.x, 2) + Math.pow(this.y - ent.y, 2)) <= 30;
+    }
+    player.prototype.hitEnemy = function(ent){
+        return Math.sqrt(Math.pow(this.x - ent.x, 2) + Math.pow(this.y - ent.y, 2)) <= 45;
+    }
 
     function ammo(x, y){
         this.x = x;
@@ -36,36 +42,119 @@ window._setEntyties = function(source, ctx, ent){
     ammo.prototype.move = function(){
         this.y -= 12;
     }
+    ammo.prototype.hit = function(ent){
+        return Math.sqrt(Math.pow(this.x - ent.x, 2) + Math.pow(this.y - ent.y, 2)) <= 30;
+    }
 
-    function enemy(x, y, r, life){
+    function eammo(x, y, vx, vy){
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.img = source.eammo;
+    }
+    eammo.prototype.draw = function(){
+        let rad = Math.atan2(this.vy, this.vx);
+        drawRotatedImage(this.img, this.x, this.y, rad + Math.PI / 2);
+    }
+    eammo.prototype.move = function(){
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+    eammo.prototype.hit = function(ent){
+        return Math.sqrt(Math.pow(this.x - ent.x, 2) + Math.pow(this.y - ent.y, 2)) <= 30;
+    }
+    eammo.prototype.outStage = function(){
+        return !(this.x >= -10 && this.x <= 410 && this.y >= -10 && this.y <= 610);
+    }
+
+    function enemy(x, y, r, life, score){
         this.x = x;
         this.y = y;
         this.r = r;
         this.life = life;
+        this.score = score;
     }
     enemy.prototype.draw = function(){
-        drawImage(this.img, this.x - 25, this.y - 25, this.r);
+        drawRotatedImage(this.img, this.x, this.y, this.r);
+    }
+    enemy.prototype.outStage = function(){
+        return !(this.x >= -25 && this.x <= 425 && this.y >= -25 && this.y <= 625);
     }
 
     function coin(pl){
-        let x, y;
-        x = ((Math.random() * 200) | 0) + pl.x < 200 ? 0 : 200;
-        y = -25;
-        enemy.call(this, x, y, 0, 1);
+        let x = ((Math.random() * 400) | 0), y = -25;
+        enemy.call(this, x, y, 0, 1, 100);
         this.img = source.coin;
         let rad = Math.atan2(pl.y - y, pl.x - x);
         this.vx = Math.cos(rad) * 7;
         this.vy = Math.sin(rad) * 7;
+        this.turned = false;
+        this.fired = false;
     }
+    Object.setPrototypeOf(coin.prototype, enemy.prototype);
     coin.prototype.update = function(pl){
         this.x += this.vx;
         this.y += this.vy;
         let flg = this.vx > 0 ? this.x > pl.x : this.x < pl.x;
-        if(flg) this.vx *= -1;
+        if(flg && !this.turned) {this.vx *= -1; this.turned = true};
+    }
+    coin.prototype.fire = function(){
+        if(!this.fired && this.turned){
+            this.fired = true;
+            return ((Math.random() * 3) | 0) === 0;
+        }
+        return false;
+    }
+
+    function kamikaze(pl){
+        enemy.call(this, pl.x, -25, 0, 3, 350);
+        this.img = source.kamikaze;
+    }
+    Object.setPrototypeOf(kamikaze.prototype, enemy.prototype);
+    kamikaze.prototype.update = function(){
+        this.y += 9;
+    }
+    kamikaze.prototype.fire = function(){
+        return ((Math.random() * 75) | 0) === 0;
+    }
+
+    function turner(pl){
+        let x = ((Math.random() * 400) | 0), y = -25;
+        this.toY = 150 + Math.random() * 150;
+        let toX = ((Math.random() * 400) | 0);
+        let rad = Math.atan2(this.toY - y, toX - x, 200);
+        enemy.call(this, x, y, rad - Math.PI / 2, 1, 200);
+        this.vx = Math.cos(rad) * 7;
+        this.vy = Math.sin(rad) * 7;
+        this.stop = 0;
+        this.stoped = false;
+        this.img = source.turner;
+    }
+    Object.setPrototypeOf(turner.prototype, enemy.prototype);
+    turner.prototype.update = function(){
+        if(this.stop == 0){
+            this.x += this.vx;
+            this.y += this.vy;
+        }else{
+            this.stop--;
+            if(this.stop == 0) this.r = Math.atan2(this.vy, this.vx) - Math.PI / 2;
+        }
+        if(this.y >= this.toY && !this.stoped){
+            this.stop = 20 + ((Math.random() * 21) | 0);
+            this.stoped = true;
+            this.vy *= -1;
+        }
+    }
+    turner.prototype.fire = function(){
+        return this.stop > 0 ? ((Math.random() * 20) | 0) === 0 : false;
     }
 
     ent.Player = player;
     ent.Coin = coin;
     ent.Ammo = ammo;
+    ent.Eammo = eammo;
+    ent.Kamikaze = kamikaze;
+    ent.Turner = turner;
 
 }
--- js/main.js
+++ js/main.js
@@ -24,9 +24,13 @@ window.addEventListener("load", () => {
         let ml = MediaLoader.create();
         ml.loadImage({
             back: "img/back.png",
+            backt: "img/backt.png",
             player: "img/player.png",
             ammo: "img/ammo.png",
-            coin: "img/coin.png"
+            eammo: "img/eammo.png",
+            coin: "img/coin.png",
+            kamikaze: "img/kamikaze.png",
+            turner: "img/turner.png"
         }, setEntyties, image);
         function setEntyties(source){
             window._setEntyties(source, ctx, ent);
@@ -40,9 +44,15 @@ window.addEventListener("load", () => {
         let frame = 0;
         let timer = setInterval(tick, 1000 / 30);
 
+        let somePushed = false;
+        let gameOverd = 0;
+
+        let score = 0;
+
         let player = new ent.Player(WIDTH / 2, HEIGHT - 100);
         let fireLast = false;
         let fireInt = 0;
+
         let plAmmo = [];
         let enemy = [];
         let enAmmo = [];
@@ -55,8 +65,9 @@ window.addEventListener("load", () => {
         }
 
         function update(){
+            if(somePushed && !keyLogger.someDown()) somePushed = false;
             if(state === 0){
-                if(keyLogger.someDown()) state = 1;
+                if(keyLogger.someDown() && !somePushed) state = 1;
                 fireInt = 10;
             }else if(state === 1){
                 let vx = 0, vy = 0;
@@ -77,8 +88,85 @@ window.addEventListener("load", () => {
                         i--;
                     }
                 }
+                let flg = (Math.random() * 25) | 0;
+                if(flg === 0){
+                    pat = (Math.random() * 10) | 0;
+                    if(pat < 5) enemy.push(new ent.Coin(player));
+                    else if(pat < 8) enemy.push(new ent.Turner(player));
+                    else enemy.push(new ent.Kamikaze(player));
+                }
+                for(var i = 0; i < enemy.length; i++){
+                    enemy[i].update(player);
+                }
+                for(var i = 0; i < enAmmo.length; i++){
+                    enAmmo[i].move();
+                    if(enAmmo[i].outStage()){
+                        enAmmo.splice(i, 1);
+                        i--;
+                        continue;
+                    }
+                    if(enAmmo[i].hit(player)){
+                        //console.log("you dead!");
+                        gameOverd = 33;
+                        state = 2;
+                        enAmmo.splice(i, 1);
+                        i--;
+                    }
+                }
+                for(var i = 0; i < plAmmo.length; i++){
+                    if(plAmmo[i].y <= -10){
+                        plAmmo.splice(i, 1);
+                        i--;
+                        continue;
+                    }
+                    for(var j = 0; j < enemy.length; j++){
+                        if(plAmmo[i].hit(enemy[j])){
+                            //console.log("hit");
+                            plAmmo.splice(i, 1);
+                            i--;
+                            enemy[j].life--;
+                            if(enemy[j].life <= 0){
+                                score += enemy[j].score;
+                                enemy.splice(j, 1);
+                                j--;
+                            }
+                            break;
+                        }
+                    }
+                }
+                for(var i = 0; i < enemy.length; i++){
+                    if(enemy[i].outStage()){
+                        enemy.splice(i, 1);
+                        i--;
+                        continue;
+                    }
+                    if(player.hitEnemy(enemy[i])){
+                        //console.log("you dead!");
+                        gameOverd = 33;
+                        state = 2;
+                        enemy.splice(i, 1);
+                        i--;
+                        continue;
+                    }
+                    if(enemy[i].fire()){
+                        let rad = Math.atan2(player.y - enemy[i].y + Math.random() * 10 - 5,
+                                             player.x - enemy[i].x + Math.random() * 10 - 5);
+                        let rx = 8 * Math.cos(rad), ry = 8 * Math.sin(rad);
+                        enAmmo.push(new ent.Eammo(enemy[i].x, enemy[i].y, rx, ry));
+                    }
+                }
             }else if(state === 2){
-
+                if(gameOverd <= 0 && keyLogger.someDown()){
+                    somePushed = true;
+                    player.x = WIDTH / 2;
+                    player.y = HEIGHT - 100;
+                    plAmmo = [];
+                    enemy = [];
+                    enAmmo = [];
+                    score = 0;
+                    state = 0;
+                }
+                if(gameOverd > 0) gameOverd--;
             }
         }
 
@@ -87,24 +175,48 @@ window.addEventListener("load", () => {
 
             drawBack();
 
-            player.draw();
+            if(state == 0 || state == 1) player.draw();
+            if(state == 1) drawScore();
 
             for(var i = 0; i < plAmmo.length; i++){
                 plAmmo[i].draw();
             }
-
+            for(var i = 0; i < enAmmo.length; i++){
+                enAmmo[i].draw();
+            }
+            for(var i = 0; i < enemy.length; i++){
+                enemy[i].draw();
+            }
+            
             if(state === 0){
                 drawMenu();
+            }else if(state === 2){
+                drawGameover();
             }
         }
 
         function drawBack(){
             ctx.drawImage(image.back, 0, 0, WIDTH, HEIGHT, 0, frame % HEIGHT, WIDTH, HEIGHT);
-            ctx.drawImage(image.back, 0, 0, WIDTH, HEIGHT, 0, -HEIGHT + frame % HEIGHT + 10, WIDTH, HEIGHT);
+            ctx.drawImage(image.back, 0, 0, WIDTH, HEIGHT, 0, -HEIGHT + frame % HEIGHT, WIDTH, HEIGHT);
+            ctx.drawImage(image.backt, 0, 0, WIDTH, HEIGHT, 0, (frame * 1.5) % HEIGHT, WIDTH, HEIGHT);
+            ctx.drawImage(image.backt, 0, 0, WIDTH, HEIGHT, 0, -HEIGHT + (frame * 1.5) % HEIGHT, WIDTH, HEIGHT);
         }
 
         function drawCont(){
 
+        }
+
+        function drawScore(){
+            ctx.save();
+            ctx.font = "20px bold";
+            ctx.fillStyle = "white";
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 1;
+            ctx.textAlign = "right";
+            ctx.textBaseline = "top";
+            //ctx.strokeText(`Score:${score}`, 390, 10);
+            ctx.fillText(`Score:${score}`, 390, 10);
+            ctx.restore();
         }
 
         function drawMenu(){
@@ -118,8 +230,33 @@ window.addEventListener("load", () => {
             ctx.lineWidth = 1;
             ctx.font = "50px bold";
             ctx.globalAlpha = 0.8;
-            ctx.fillText("Push to start", WIDTH / 2, HEIGHT / 2);
-            ctx.strokeText("Push to start", WIDTH / 2, HEIGHT / 2);
+            ctx.fillText("雑シューティング", WIDTH / 2, HEIGHT / 2 - 25);
+            ctx.strokeText("雑シューティング", WIDTH / 2, HEIGHT / 2 - 25);
+            ctx.font = "35px bold";
+            ctx.fillText("Push to start", WIDTH / 2, HEIGHT / 2 + 25);
+            ctx.strokeText("Push to start", WIDTH / 2, HEIGHT / 2 + 25);
+            ctx.restore();
+        }
+
+        function drawGameover(){
+            ctx.save();
+            ctx.globalAlpha = 0.3;
+            ctx.fillStyle = "white";
+            ctx.fillRect(0, 0, WIDTH, HEIGHT);
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 1;
+            ctx.font = "50px bold";
+            ctx.globalAlpha = 0.8;
+            ctx.fillText("Game Over!", WIDTH / 2, HEIGHT / 2 - 45);
+            ctx.strokeText("Game Over!", WIDTH / 2, HEIGHT / 2 - 45);
+            ctx.font = "40px bold";
+            ctx.fillText(`Your score:${score}`, WIDTH / 2, HEIGHT / 2);
+            ctx.strokeText(`Your score:${score}`, WIDTH / 2, HEIGHT / 2);
+            ctx.font = "35px bold";
+            ctx.fillText("push to back title", WIDTH / 2, HEIGHT / 2 + 40);
+            ctx.strokeText("push to back title", WIDTH / 2, HEIGHT / 2 + 40);
             ctx.restore();
         }
 
